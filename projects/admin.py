e5a3090a2ca58d610397bd149b006f2a4f5211a5
jkingz <jfkrule@gmail.com>
2017-05-03 13:52:30 +0800

    Fixed projects

--- projects/admin.py
+++ projects/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from . models import Projects, WorkDiary
+from . models import Projects
+
 
 
 class ProjectsAdmin(admin.ModelAdmin):
--- projects/views.py
+++ projects/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from . models import Projects, WorkDiary
+from . forms import WorkDiaryForm
 
 
 class ProjectView(LoginRequiredMixin, TemplateView):
@@ -20,12 +22,19 @@ class ProjectView(LoginRequiredMixin, TemplateView):
 
 class WorkDiaryView(TemplateView):
 
+    form_class = WorkDiaryForm
     model = WorkDiary
     template_name = 'project/work_diary.html'
 
     def get(self, request, *args, **kwargs):
-        work = WorkDiary.objects.all()
-        ctx_data = {
-            'works': work,
-        }
-        return render(self.request, self.template_name, ctx_data)
+        form = self.form_class()
+        return render(self.request, self.template_name, {'form': form})
+
+
+class Reports(TemplateView):
+
+    template_name = 'project/work_detail.html'
+
+    def get(self, request, *args, **kwargs):
+        work = WorkDiary.objects.all().order_by('-date')
+        return render(request, self.template_name, {'work': work})
