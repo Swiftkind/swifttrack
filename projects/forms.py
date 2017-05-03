30fdfc75f2129027a7a3b9961681485a7164bff8
jkingz <jfkrule@gmail.com>
2017-05-03 14:38:45 +0800

    Added form on work diary feature

--- projects/forms.py
+++ projects/forms.py
@@ -7,8 +7,15 @@ class WorkDiaryForm(forms.ModelForm):
     class Meta:
         model =  WorkDiary
         fields = [
+<<<<<<< d0726ed2ff90c6479f493efe32061c8c2d7e36ec
             'user',
             'description',
             'hours',
             'project_id'
         ]
+=======
+            'description',
+            'hours',
+            'project_id',
+        ]
+>>>>>>> Added form on work diary feature
--- projects/views.py
+++ projects/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from . models import Projects, WorkDiary
+from . forms import WorkDiaryForm
 
 
 class ProjectView(LoginRequiredMixin, TemplateView):
@@ -24,8 +26,20 @@ class WorkDiaryView(TemplateView):
     template_name = 'project/work_diary.html'
 
     def get(self, request, *args, **kwargs):
+        form = WorkDiaryForm()
         work = WorkDiary.objects.all()
         ctx_data = {
             'works': work,
+            'form': form,
         }
         return render(self.request, self.template_name, ctx_data)
+<<<<<<< d0726ed2ff90c6479f493efe32061c8c2d7e36ec
+=======
+
+    def post(self, request, *args, **kwargs):
+        form = WorkDiaryForm()
+        if form.is_valid():
+            form.save()
+            return redirect('/project/')
+        return HttpResponse('Error!')
+>>>>>>> Added form on work diary feature
