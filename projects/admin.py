dff5d8aa16ec53bf57c0ea19ecccd289d8cf9bc7
jkingz <jfkrule@gmail.com>
2017-05-10 12:38:58 +0800

    Fixed work diary feature templates

--- projects/admin.py
+++ projects/admin.py
@@ -1,17 +1,22 @@
 from django.contrib import admin
-from . models import Projects, WorkDiary
+from . models import Project, ProjectAssignment, WorkDiary
 
 
 class ProjectsAdmin(admin.ModelAdmin):
 
-    list_display = ('name', 'date', 'hours',)
-    list_filter = ['date', 'hours']
+    list_display = ['name', 'date_created',]
+    list_filter = ['date_created']
 
+
+class ProjectAssignmentAdmin(admin.ModelAdmin):
+
+    list_display = ['employee', 'project', 'weekly_hours']
 
 class WorkDiaryAdmin(admin.ModelAdmin):
 
-    list_display = ('description', 'date', 'hours', )
+    list_display = ('finished_task', 'date', 'hours', )
     list_filter = ['date', 'hours']
 
-admin.site.register(Projects, ProjectsAdmin)
+admin.site.register(Project, ProjectsAdmin)
 admin.site.register(WorkDiary, WorkDiaryAdmin)
+admin.site.register(ProjectAssignment, ProjectAssignmentAdmin)
--- projects/models.py
+++ projects/models.py
@@ -3,13 +3,11 @@ from django.db import models
 from accounts.models import Account
 
 
-class Projects(models.Model):
+class Project(models.Model):
 
-    user = models.ForeignKey(Account, on_delete=models.CASCADE)
+    user = models.ForeignKey(Account, null=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
-    date = models.DateTimeField(auto_now=True)
-    hours = models.FloatField()
-    hours_spent = models.FloatField()
+    date_created = models.DateTimeField(auto_now_add=True)
 
 
     class Meta:
@@ -20,13 +18,24 @@ class Projects(models.Model):
         return self.name
 
 
+class ProjectAssignment(models.Model):
+
+    employee = models.ForeignKey(Account, null=True, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, null=True, on_delete=models.CASCADE)
+    weekly_hours = models.FloatField()
+
+    class Meta:
+        verbose_name_plural = "project-assignment"
+
+
 class WorkDiary(models.Model):
 
-    user = models.ForeignKey(Account, on_delete=models.CASCADE)
-    description = models.TextField()
+    project_assignment = models.ForeignKey(ProjectAssignment, null=True, on_delete=models.CASCADE)
+    finished_task = models.TextField()
+    todo_task = models.TextField()
+    issues = models.TextField()
     date = models.DateTimeField(auto_now=True)
     hours = models.FloatField()
-    project = models.ForeignKey(Projects, null=True, on_delete=models.CASCADE)
 
 
     class Meta:
@@ -34,4 +43,4 @@ class WorkDiary(models.Model):
 
 
     def __str__(self):
-        return self.description
+        return self.finished_task
