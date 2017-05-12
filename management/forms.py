8ac1899c4d9b16295cf6516ab241c6d4a3ef6efe
regzimarx <gergimarjohnalinsangao@gmail.com>
2017-05-12 10:29:08 +0800

    Added work diaries to home page of management

--- management/forms.py
+++ management/forms.py
@@ -3,16 +3,16 @@ from .models import Requests
 from datetime import datetime
 
 class RequestForm(ModelForm):
-	class Meta:
-		model = Requests
-		fields = ('employee', 'subject', 'date_of_leave', 'date_of_return', 'content')
-		widgets = {
+    class Meta:
+        model = Requests
+        fields = ('employee', 'subject', 'date_of_leave', 'date_of_return', 'content')
+        widgets = {
             'employee': widgets.TextInput(attrs={'type': 'hidden'}),
             'date_of_leave': widgets.DateTimeInput(attrs={'class':'form-control'}),
             'date_of_return': widgets.DateTimeInput(attrs={'class':'form-control'}),
         }
 
-	def __init__(self, *args, **kargs):
-	    super(RequestForm, self).__init__(*args, **kargs)
-	    self.fields['subject'].widget.attrs.update({'class' : 'form-control'})
-	    self.fields['content'].widget.attrs.update({'class' : 'form-control'})
+    def __init__(self, *args, **kargs):
+        super(RequestForm, self).__init__(*args, **kargs)
+        self.fields['subject'].widget.attrs.update({'class' : 'form-control'})
+        self.fields['content'].widget.attrs.update({'class' : 'form-control'})
--- management/urls.py
+++ management/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     url(r'^make-request$', views.RequestView.as_view(), name='request'),
     url(r'^request-status$', views.UpdateRequest.as_view(), name='update_request'),
-    url(r'^dashboard$', views.AdminView.as_view(), name='admin'),
+    url(r'^dashboard/(?P<day>[0-9]+)/(?P<di>[\w-]+)/$', views.AdminView.as_view(), name='admin'),
+    #url(r'^dashboard/(?P<day>)', views.AdminWorkDiariesView.as_view(), name='admin_work_diaries'),
     url(r'^confirm-account', views.ConfirmAccountView.as_view(), name='confirm_account'),
     url(r'^deactivate-account', views.DeactivateAccountView.as_view(), name='deactivate_account'),
     url(r'^employees/$', views.AllEmployeesView.as_view(), name='all_employees'),
