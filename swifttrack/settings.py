4d5b08d5146a677fbf98a26a640a539d83abb77a
root <root@track.swiftkind.com>
2017-05-19 07:07:56 +0000

    fix static files

--- swifttrack/settings.py
+++ swifttrack/settings.py
@@ -121,15 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_PATH = os.path.join(BASE_DIR, '../staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "assets"),
+]
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-
-STATICFILES_DIRS = (
-    STATIC_PATH,
-)
 
 AUTH_USER_MODEL = 'accounts.Account'
 LOGIN_REDIRECT_URL = '/project/'
