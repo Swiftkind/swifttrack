ef54bc69d5faaf4e64a381153be46410eafbaad2
jkingz <jfkrule@gmail.com>
2017-05-24 13:21:50 +0800

    Fix broken redirect

--- accounts/views.py
+++ accounts/views.py
@@ -61,7 +61,7 @@ class LogoutView(TemplateView):
 
     def get(self, request):
         logout(request)
-        return redirect('/')
+        return redirect('/project/account/')
 
 # Account view
 
