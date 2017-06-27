3e980542a3e88b658799669962c3de8443b28ef7
jkingz <jfkrule@gmail.com>
2017-06-27 13:03:24 +0800

    Create new models and new migrations file

--- management/admin.py
+++ management/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Requests
+from .models import Requests, Misc
 
-# Register your models here.
+
+class MiscAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'types', 'total_amount', 'months', 'date_created', 'status']
+    list_filter = ('types', 'date_created',)
+
 admin.site.register(Requests)
+admin.site.register(Misc, MiscAdmin)
