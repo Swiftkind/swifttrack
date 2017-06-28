be8b478bdb23cdc5eab9cec20abf77d68cf5117d
jkingz <jfkrule@gmail.com>
2017-06-28 15:01:39 +0800

    Create misc details page for users employee

--- projects/urls.py
+++ projects/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
-from .views import ProjectView, WorkDiaryView, WorkDiaryEditView
+from .views import ProjectView, WorkDiaryView, WorkDiaryEditView, EmployeesMiscView
 from . import views
 
 urlpatterns = [
     url(r'^$', ProjectView.as_view(), name='project'),
     url(r'^work_diary/(?P<id>[0-9]+)/$', WorkDiaryView.as_view(), name='work-diary'),
     url(r'^work_diary/(?P<id>[0-9]+)/edit/(?P<work_diary_id>[0-9]+)/$', WorkDiaryEditView.as_view(), name='edit-work-diary'),
+    # url(r'^project/misc/(?P<misc_id>[0-9]+)/$', MiscEmployeeView.as_view(), name='employees_misc'),
+    url(r'^misc/$', EmployeesMiscView.as_view(), name='employees_misc'),
 ]
